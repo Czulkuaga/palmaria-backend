8ea7003e64cc69995763dc7f43ede5ab69e20f94
Cesar Zuluaga <cesar.zuluaga@aliatic.com.co>
2023-10-19 09:48:32 -0500

    cesar.zuluaga 2023.10.19 add recaptcha verification

--- controller/ScrapingController.js
+++ controller/ScrapingController.js
@@ -3,21 +3,33 @@ const axios = require('axios');
 const InterlocutorService = require('../services/InterlocutorService')
 // const cheerio = require('cheerio');
 
+const verifyCaptchUrl = "https://www.google.com/recaptcha/api/siteverify"
+
 ScrapingController.obtainCbml = async (req, res) => {
-    const url = req.body.url
-    const params = req.body.data.matricula
-    try {
-        const response = await axios.get(`${url}${params}`); // Reemplaza con la URL del sitio web que deseas scrape
-        const html = response.data;
-        // console.log(html)
+  const url = req.body.url
+  const params = req.body.data.matricula
+  try {
+    const verifyCaptcha = await axios.post(`${verifyCaptchUrl}?secret=${process.env.RE_CAPTCHA_GOOGLE}&response=${req.body.data.captcha}`)
+    const resCaptcha = verifyCaptcha.data
+    console.log(resCaptcha)
 
-        await InterlocutorService.saveFormData(req.body.data)
+    if (resCaptcha.success === true) {
+      const response = await axios.get(`${url}${params}`); // Reemplaza con la URL del sitio web que deseas scrape
+      const html = response.data;
+      // console.log(html)
 
-        res.json({code:'200', msg:"Información encontrada", data: html})
-      } catch (error) {
-        console.error('Error al hacer la solicitud HTTP:', error);
-        res.json({code:'500', msg:"Hubo un error en el servicio"})
-      }
+      await InterlocutorService.saveFormData(req.body.data)
+
+      res.json({ code: '200', msg: "Información encontrada", data: html })
+    }else{
+      res.json({ code: '500', msg: "No se pudo validar el captcha", data: null })
+    }
+
+
+  } catch (error) {
+    console.error('Error al hacer la solicitud HTTP:', error);
+    res.json({ code: '500', msg: "Hubo un error en el servicio" })
+  }
 }
 
 module.exports = ScrapingController
